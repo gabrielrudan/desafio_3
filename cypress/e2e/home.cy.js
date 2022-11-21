94c805c591cd6d040b04391ae68000823a57b589
Gabriel Rudan <gabrielrudan@alu.ufc.br>
2022-11-21 16:27:24 -0300

    Primeiro commit

---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,7 @@
+describe('Tests in my home page', () => {
+
+    it('visit my home', () => {
+        cy.visit('http://localhost:3000/')
+    });
+
+});
---
+++ src/pages/Home/index.js
@@ -0,0 +1,9 @@
+import './style.module.scss';
+
+export function Home() {
+  return (
+    <div className="container-home">
+      
+    </div>
+  );
+}
