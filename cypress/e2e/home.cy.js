aa81a54c345aa1ce4b5521294ad658c2bbc5ace4
Gabriel Rudan <gabrielrudan@alu.ufc.br>
2022-12-02 23:09:29 -0300

    Desafio 2 concluido

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,7 +1,44 @@
 describe('Tests in my home page', () => {
 
-    it('visit my home', () => {
+    beforeEach(() => {
         cy.visit('http://localhost:3000/')
+    });
+
+    it('type in my input and add in my list', () => {
+        cy.get('#input-name').type('Gabriel Rudan');
+        cy.get('button').click();
+
+        cy.get('li').first().should('contain', 'Gabriel Rudan')
+    });
+
+    it('insert three names in my list', () => {
+        cy.get('#input-name').type('Gabriel Rudan');
+        cy.get('button').click();
+
+        cy.get('#input-name').type('Letícia Torres');
+        cy.get('button').click();
+
+        cy.get('#input-name').type('Raylander Marques');
+        cy.get('button').click();
+
+        cy.get('li').first().should('contain', 'Gabriel Rudan')
+        cy.get('li').last().should('contain', 'Raylander Marques')
+    });
+
+    it('try to insert null name', () => {
+        cy.get('button').click();
+        cy.get('#empty-list').first().should('contain', 'Lista de Nomes Vazia...')
+        
+    });
+
+    it('try to insert the same name', () => {
+        cy.get('#input-name').type('Alysson Araujo');
+        cy.get('button').click();
+
+        cy.get('#input-name').type('Alysson Araujo');
+        cy.get('button').click();
+
+        cy.get('#same-error').first().should('contain', 'Esse nome já está na lista')
     });
 
 });
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,59 @@
-import './style.module.scss';
+import { useState } from 'react';
+import style from './style.module.scss';
 
-export function Home() {
+export default function Home() {
+
+  const [name, setName] = useState('');
+  const [nameList, setNameList] = useState([]);
+  const [error, setError] = useState('');
+
+  function handleAddNameInList(){
+    if(!name){
+      setError('Digite um nome primeiro...');
+      return;
+    }
+
+    const findName = nameList.findIndex((item) => item===name);
+
+    if(findName !== -1){
+      setError('Esse nome já está na lista')
+      return;
+    }
+
+    setNameList([...nameList, name]);
+    setName('');
+    setError('');
+  }
+
   return (
-    <div className="container-home">
-      
+    <div className={style.container}>
+      <div>
+      <h2>Inserindo nomes</h2>
+        <input type='text'
+        placeholder='Name'
+        id='input-name'
+        value={name}
+          onChange={(e)=> setName(e.target.value)}
+        />
+        <button
+        className={style.btnInsert}
+        onClick={() => handleAddNameInList()}
+        >
+          Adicionar
+        </button>
+        <span id='same-error'>{error}</span>
+      </div>
+      <div>
+        {nameList.length ?
+          <ul>
+            {nameList.map((item) => (
+              <li key={item}>{item}</li>
+            ))}
+          </ul>
+        :
+        <span id='empty-list'>Lista de Nomes Vazia...</span>
+        }
+      </div>
     </div>
   );
 }
